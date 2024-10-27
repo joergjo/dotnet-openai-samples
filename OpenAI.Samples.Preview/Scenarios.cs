f08ec70c7573d2710b52626de5a609ad5b81be42
Jörg Jooss <joerg@joergjooss.de>
2024-10-27 07:09:45 +0100

    Add SpeechGenerationOptions

--- OpenAI.Samples.Preview/Scenarios.cs
+++ OpenAI.Samples.Preview/Scenarios.cs
@@ -6,7 +6,12 @@ public static class Scenarios
 {
     public static (string, string) TextToSpeech(AudioClient client, string text, string file)
     {
-        var result = client.GenerateSpeech(text, GeneratedSpeechVoice.Onyx);
+        var options = new SpeechGenerationOptions
+        {
+            ResponseFormat = GeneratedSpeechFormat.Mp3,
+            SpeedRatio = 1.0f
+        };
+        var result = client.GenerateSpeech(text, GeneratedSpeechVoice.Alloy, options);
         using var fileStream = File.OpenWrite(file);
         result.Value.ToStream().CopyTo(fileStream);
         return (text, $"Audio saved to {file}");
