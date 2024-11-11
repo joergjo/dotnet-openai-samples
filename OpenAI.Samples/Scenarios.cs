2ce03675e9a6f1d91851179e1254e9fc47bbc48e
Jörg Jooss <joergjo@microsoft.com>
2024-11-11 18:02:42 +0100

    Remove native code dependencies for now

--- OpenAI.Samples/Scenarios.cs
+++ OpenAI.Samples/Scenarios.cs
@@ -1,4 +1,3 @@
-using Emgu.CV;
 using OpenAI.Audio;
 using OpenAI.Chat;
 
@@ -114,52 +113,52 @@ public static class Scenarios
         return (refinedPrompt, completion.Content[0].Text);
     }
 
-    public static (string, string) SummarizeVideo(ChatClient chatClient, string srcFile, int start = 0, int count = 50)
-    {
-        var frame = new Mat();
-        var encodedFrames = new List<string>();
-        var frameCount = 0;
-        var last = start + count;
-        using var capture = new VideoCapture(srcFile);
-        while (capture.Read(frame) && frameCount < last)
-        {
-            if (frame.IsEmpty) break;
-            if (frameCount++ < start) continue;
+    // public static (string, string) SummarizeVideo(ChatClient chatClient, string srcFile, int start = 0, int count = 50)
+    // {
+    //     var frame = new Mat();
+    //     var encodedFrames = new List<string>();
+    //     var frameCount = 0;
+    //     var last = start + count;
+    //     using var capture = new VideoCapture(srcFile);
+    //     while (capture.Read(frame) && frameCount < last)
+    //     {
+    //         if (frame.IsEmpty) break;
+    //         if (frameCount++ < start) continue;
 
-            var buffer = CvInvoke.Imencode(".jpg", frame);
-            if (buffer is null) throw new Exception("Failed to encode frame");
-            var frameBase64 = $$"""{"image":"{{Convert.ToBase64String(buffer)}}", "resize":768""";
-            encodedFrames.Add(frameBase64);
-        }
+    //         var buffer = CvInvoke.Imencode(".jpg", frame);
+    //         if (buffer is null) throw new Exception("Failed to encode frame");
+    //         var frameBase64 = $$"""{"image":"{{Convert.ToBase64String(buffer)}}", "resize":768""";
+    //         encodedFrames.Add(frameBase64);
+    //     }
 
-        var userPrompt = $"""
-                          These are the first 50 frames of a video. Generate a two sentence summary.
-                          {string.Join(" ", encodedFrames)}
-                          """;
+    //     var userPrompt = $"""
+    //                       These are the first 50 frames of a video. Generate a two sentence summary.
+    //                       {string.Join(" ", encodedFrames)}
+    //                       """;
         
-        ChatCompletion completion = chatClient.CompleteChat(
-        [
-            new UserChatMessage(userPrompt),
-        ]);
-        return ($"These are the first 50 frames of a video. Generate a two sentence summary.",
-            completion.Content[0].Text);
-    }
+    //     ChatCompletion completion = chatClient.CompleteChat(
+    //     [
+    //         new UserChatMessage(userPrompt),
+    //     ]);
+    //     return ($"These are the first 50 frames of a video. Generate a two sentence summary.",
+    //         completion.Content[0].Text);
+    // }
     
-    public static void PrepareVideoFrames(string source, string outputDir = ".")
-    {
-        if (!Directory.Exists(outputDir)) throw new Exception("Destination directory does not exist.");
+    // public static void PrepareVideoFrames(string source, string outputDir = ".")
+    // {
+    //     if (!Directory.Exists(outputDir)) throw new Exception("Destination directory does not exist.");
         
-        var frameCount = 0;
-        var frame = new Mat();
-        using var capture = new VideoCapture(source);
-        while (capture.Read(frame))
-        {
-            if (frame.IsEmpty) break;
+    //     var frameCount = 0;
+    //     var frame = new Mat();
+    //     using var capture = new VideoCapture(source);
+    //     while (capture.Read(frame))
+    //     {
+    //         if (frame.IsEmpty) break;
             
-            var frameFile = Path.Combine(outputDir, $"frame{frameCount++}.jpg");
-            var success = CvInvoke.Imwrite(frameFile, frame);
-            if (!success) throw new Exception("Failed to write frame"); 
-            Console.WriteLine($"Saved {frameFile}");
-        }
-    }
+    //         var frameFile = Path.Combine(outputDir, $"frame{frameCount++}.jpg");
+    //         var success = CvInvoke.Imwrite(frameFile, frame);
+    //         if (!success) throw new Exception("Failed to write frame"); 
+    //         Console.WriteLine($"Saved {frameFile}");
+    //     }
+    // }
 }
